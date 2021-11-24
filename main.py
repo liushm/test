08660730706fec1d86f73e6b5665fc15ea673793
liushm <liushm.cn@gmail.com>
2021-11-24 09:25:05 +0800

    update.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 #!/bin/env python3
 
+from urllib import request
+
 if __name__ == '__main__':
     print('test git reflog')
+
+    req = request.urlopen('https://afan.ml/')
+    print(req.read().decode())
+
